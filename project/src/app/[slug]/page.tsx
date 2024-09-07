14c8234083d00f0a4e3c60a74ba50ca72a07fe41
Kalana Dias <74653324+diaskalana@users.noreply.github.com>
2024-09-07 16:34:11 +0530

    Implement single product page

--- project/src/app/[slug]/page.tsx
+++ project/src/app/[slug]/page.tsx
@@ -1,60 +1,71 @@
 import Add from "@/components/Add";
 import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
+import { wixClientServer } from "@/lib/wixClientServer";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
-const SinglePage = async () => {
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+  const wixClient = await wixClientServer();
+
+  const products = await wixClient.products
+    .queryProducts()
+    .eq("slug", params.slug)
+    .find();
+
+  if (!products.items[0]) {
+    return notFound();
+  }
+
+  const product = products.items[0];
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
       {/* TEXTS */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">Product Name</h1>
-        <p className="text-gray-500">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet blanditiis 
-        pariatur consequuntur ullam nostrum voluptates, molestias eius ipsam odit fugiat?
-        </p>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
         <div className="h-[2px] bg-gray-100" />
-        <div className="flex items-center gap-4">
-          <h3 className="text-xl text-gray-500 line-through">
-            $59
-          </h3>
-          <h2 className="font-medium text-2xl">
-            $49
-          </h2>
-        </div>
+        {product.price?.price === product.price?.discountedPrice ? (
+          <h2 className="font-medium text-2xl">${product.price?.price}</h2>
+        ) : (
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">
+              ${product.price?.price}
+            </h3>
+            <h2 className="font-medium text-2xl">
+              ${product.price?.discountedPrice}
+            </h2>
+          </div>
+        )}
         <div className="h-[2px] bg-gray-100" />
-          <CustomizeProducts/>
-          <Add/>
+        {product.variants && product.productOptions ? (
+          <CustomizeProducts
+            productId={product._id!}
+            variants={product.variants}
+            productOptions={product.productOptions}
+          />
+        ) : (
+          <Add
+            productId={product._id!}
+            variantId="00000000-0000-0000-0000-000000000000"
+            stockNumber={product.stock?.quantity || 0}
+          />
+        )}
         <div className="h-[2px] bg-gray-100" />
-          <div className="text-sm">
-            <h4 className="font-medium mb-4">Title</h4>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet tempora, 
-            quam suscipit ex fugit possimus corporis earum pariatur praesentium hic 
-            dicta asperiores, ullam maiores deleniti in! Ex recusandae voluptatibus blanditiis?
-            </p>
+        {product.additionalInfoSections?.map((section: any) => (
+          <div className="text-sm" key={section.title}>
+            <h4 className="font-medium mb-4">{section.title}</h4>
+            <p>{section.description}</p>
           </div>
-          <div className="text-sm">
-            <h4 className="font-medium mb-4">Title</h4>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet tempora, 
-            quam suscipit ex fugit possimus corporis earum pariatur praesentium hic 
-            dicta asperiores, ullam maiores deleniti in! Ex recusandae voluptatibus blanditiis?
-            </p>
-          </div>
-          <div className="text-sm">
-            <h4 className="font-medium mb-4">Title</h4>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet tempora, 
-            quam suscipit ex fugit possimus corporis earum pariatur praesentium hic 
-            dicta asperiores, ullam maiores deleniti in! Ex recusandae voluptatibus blanditiis?
-            </p>
-          </div>
+        ))}
+        <div className="h-[2px] bg-gray-100" />
+        {/* REVIEWS */}
+        <h1 className="text-2xl">User Reviews</h1>
       </div>
     </div>
   );
