b4058a1f9213b8d0cf2d74727e420afe62cd54e7
Kalana Dias <74653324+diaskalana@users.noreply.github.com>
2024-09-07 15:50:49 +0530

    Implement category pages

--- project/src/app/list/page.tsx
+++ project/src/app/list/page.tsx
@@ -1,9 +1,17 @@
 import Filter from "@/components/Filter";
 import ProductList from "@/components/ProductList";
+import Skeleton from "@/components/Skeleton";
+import { wixClientServer } from "@/lib/wixClientServer";
 import Image from "next/image";
 import { Suspense } from "react";
 
-const ListPage = async () => {
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
+
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* CAMPAIGN */}
@@ -13,7 +21,7 @@ const ListPage = async () => {
             Grab up to 50% off on
             <br /> Selected Products
           </h1>
-          <button className="rounded-3xl bg-primary text-white w-max py-3 px-5 text-sm">
+          <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
             Buy Now
           </button>
         </div>
@@ -24,8 +32,17 @@ const ListPage = async () => {
       {/* FILTER */}
       <Filter />
       {/* PRODUCTS */}
-      <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
-      <ProductList/>
+      <h1 className="mt-12 text-xl font-semibold">
+        {cat?.collection?.name} For You!
+      </h1>
+      <Suspense fallback={<Skeleton />}>
+        <ProductList
+          categoryId={
+            cat.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
     </div>
   );
 };
