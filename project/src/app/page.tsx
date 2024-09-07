e07544297cfe65559efb5b9a6335523ae21b6a98
Kalana Dias <74653324+diaskalana@users.noreply.github.com>
2024-09-07 14:33:32 +0530

    Implement skeleton component

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,14 +1,21 @@
-import CategoryList from "@/components/CategoryList"
-import ProductList from "@/components/ProductList"
-import Slider from "@/components/Slider"
+import CategoryList from "@/components/CategoryList";
+import ProductList from "@/components/ProductList";
+import Skeleton from "@/components/Skeleton";
+import Slider from "@/components/Slider";
+import { Suspense } from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
   return (
-    <div className=''>
+    <div className="">
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <ProductList />
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_FEATURED_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
       <div className="mt-24">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
@@ -17,7 +24,7 @@ const HomePage = () => {
         <CategoryList />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
